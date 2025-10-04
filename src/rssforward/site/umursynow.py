575754bc855a8909176a1f0108f69a2ddcd07ee1
anetczuk <anetczuk@o2.pl>
2025-10-04 23:34:22 +0200

    Add UM Ursynów scrapper

---
+++ src/rssforward/site/umursynow.py
@@ -0,0 +1,291 @@
+#
+# Copyright (c) 2025, Arkadiusz Netczuk <dev.arnet@gmail.com>
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+# pylint: disable=E0401 (import-error)
+
+import logging
+import time
+from typing import Dict
+import datetime
+
+import random
+from urllib.parse import urljoin
+import requests
+
+from bs4 import BeautifulSoup
+from feedgen.feed import FeedGenerator
+
+from rssforward.utils import normalize_string, write_data
+from rssforward.rssgenerator import RSSGenerator
+from rssforward.rss.utils import init_feed_gen, dumps_feed_gen, add_data_to_feed
+from rssforward.site.utils.htmlbuild import convert_line, convert_list, convert_title, convert_content
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+MAIN_NAME = "UM Ursynów"
+MAIN_URL = "https://ursynow.um.warszawa.pl"
+
+
+#
+class UMUrsynowGenerator(RSSGenerator):
+
+    def authenticate(self, _login, _password):
+        return True
+
+    def generate(self) -> Dict[str, str]:
+        _LOGGER.info(f"========== running {MAIN_NAME} scraper ==========")
+        content = get_content()
+        return {"news.xml": content}
+
+
+def get_content(items_num=20, html_output=None):
+    news_links = get_news_links(items_num, False)
+    if not news_links:
+        return None
+
+    feed_gen: FeedGenerator = init_feed_gen(MAIN_URL)
+    feed_gen.title(MAIN_NAME)
+    feed_gen.description(MAIN_NAME)
+
+    for full_url in news_links:
+        add_news(feed_gen, full_url, html_output)
+
+    try:
+        content = dumps_feed_gen(feed_gen)
+        return content
+    except ValueError:
+        _LOGGER.error(f"unable to dump feed, content:\n{feed_gen}")
+        raise
+
+
+def get_news_links(posts_num=9999, throw=True):
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"}
+    response = requests.get("https://ursynow.um.warszawa.pl/kalendarz?delta=75", headers=headers, timeout=10)
+
+    if response.status_code not in (200, 204):
+        if throw:
+            raise RuntimeError(f"unable to get data: {response.status_code}")
+        return None
+
+    content = response.content
+    content = content.decode("utf-8")
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    full_list = []
+    articles_list = soup.find_all("li", attrs={"class": "search-results__item"})
+    for article in articles_list:
+        article_header = article.find_all("h2", attrs={"class": "search-results__title"})[0]
+        link = article_header.select("a")[0]
+        item_url = link["href"]
+        full_url = urljoin(MAIN_URL, item_url)
+        full_list.append(full_url)
+
+    items_num = min(posts_num, len(full_list))
+    full_list = full_list[0:items_num]
+    return full_list
+
+
+def add_news(feed_gen, full_url, html_output=None):
+    offer_data = extract_news_data(full_url)
+    if not offer_data:
+        return
+    if html_output:
+        _LOGGER.info("writing html content to file: %s", html_output)
+        content = offer_data["content"]
+        write_data(html_output, content)
+    add_data_to_feed(feed_gen, offer_data)
+
+
+def extract_news_data(news_url=None, content=None):
+    # sleep_random(3)
+    if news_url is not None:
+        _LOGGER.info(f"getting offer details: {news_url}")
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"}
+        response = requests.get(news_url, headers=headers, timeout=10)
+
+        if response.status_code not in (200, 204):
+            _LOGGER.warning("unable to get job offer content")
+            return None
+
+        content = response.content
+        content = content.decode("utf-8")
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    id_value = news_url.rsplit("/", 1)[-1]
+
+    article = soup.find_all("div", attrs={"id": "column-2"})[0]
+
+    article_title_list = article.find_all("h1", attrs={"class": "article-title"})
+    if not article_title_list:
+        _LOGGER.warning("unable to get event from: %s", news_url)
+        return None
+    title_text = article_title_list[0].text
+
+    article_element_list = article.find_all("button")
+    for item in article_element_list:
+        item.clear()
+
+    article_element = article.find_all("span", attrs={"class": "header-back-to"})[0]
+    article_element.clear()
+
+    article_element = article.find_all("div", attrs={"class": "article-tools"})[0]
+    article_element.clear()
+
+    item_desc = str(article)
+    item_desc = normalize_string(item_desc)
+    # item_desc = convert_to_html(item_desc)
+
+    utc_dt = datetime.datetime.now(datetime.timezone.utc)
+    item_date = utc_dt.astimezone()
+
+    content = f"""\
+<style>
+
+</style>
+
+{item_desc}
+"""
+
+    return {
+        "id": id_value,
+        "title": title_text,
+        "author": {"name": MAIN_NAME, "email": MAIN_NAME},
+        "content": item_desc,
+        "pub_date": item_date,
+        "link": news_url,
+    }
+
+
+def convert_work_schedule(work_schedule_list):
+    content_list = []
+    for item in work_schedule_list:
+        content_list.append(item["name"])
+    if not content_list:
+        return ""
+    output = convert_list("Wymiar godzin:", content_list)
+    return f"""{output}<br/>\n"""
+
+
+def convert_contract_data(contract_data_list):
+    content_list = []
+    for item in contract_data_list:
+        item_content = item["name"]
+        salary = item["salary"]
+        if salary:
+            item_content += f""" {salary["from"]} - {salary["to"]}"""
+            item_content += f""" {salary["currency"]["code"]} {salary["timeUnit"]["shortForm"]["name"]}"""
+            item_content += f""" {salary["salaryKind"]["name"]}"""
+        content_list.append(item_content)
+    if not content_list:
+        return ""
+    output = convert_list("Rodzaj umowy:", content_list)
+    return f"""{output}<br/>\n"""
+
+
+def convert_work_mode(work_mode_list):
+    content_list = []
+    for item in work_mode_list:
+        content_list.append(item["name"])
+    if not content_list:
+        return ""
+    output = convert_list("Tryb pracy:", content_list)
+    return f"""{output}<br/>\n"""
+
+
+def convert_to_section(section_data):
+    title = section_data.get("title")
+    if title is None:
+        return ""
+
+    sub_sec_list = []
+
+    model = section_data.get("model")
+    if model is not None:
+        model_content = convert_model(model)
+        if model_content:
+            sub_sec_list.append(model_content)
+
+    sub_sections = section_data.get("subSections")
+    if sub_sections is not None:
+        for subsect in sub_sections:
+            sub_content = convert_to_section(subsect)
+            if sub_content:
+                sub_sec_list.append(sub_content)
+
+    if not sub_sec_list:
+        return ""
+
+    section_content = "<br/>\n".join(sub_sec_list)
+
+    ret_content = ""
+    if title:
+        ret_content += """<!-- convert_to_section --><div style="margin-left: 12px">"""
+        ret_content += convert_title(title)
+        ret_content += section_content
+        ret_content += "</div>\n"
+    else:
+        ret_content += f"{section_content}\n"
+    return ret_content
+
+
+def convert_model(model_data):
+    title = None
+    model_type = model_data["modelType"]
+
+    if model_type == "open-dictionary":
+        # section_name = model_data["dictionaryName"]
+        items_list = []
+        section_data = model_data["customItems"]
+        items_list.extend([item_dict["name"] for item_dict in section_data])
+        section_data = model_data["items"]
+        items_list.extend([item_dict["name"] for item_dict in section_data])
+        return convert_line(title, items_list, inline=True)
+
+    if model_type == "open-dictionary-with-icons":
+        # section_name = model_data["dictionaryName"]
+        items_list = []
+        section_data = model_data["customItems"]
+        items_list.extend([item_dict["name"] for item_dict in section_data])
+        section_data = model_data["items"]
+        items_list.extend([item_dict["name"] for item_dict in section_data])
+        return convert_list(title, items_list)
+
+    if model_type == "bullets":
+        section_data = model_data["bullets"]
+        return convert_list(title, section_data)
+
+    if model_type == "percentages":
+        section_data = model_data["parts"]
+        items_list = [f"""{item_dict["percent"]}% {item_dict["text"]}""" for item_dict in section_data]
+        return convert_list(title, items_list)
+
+    if model_type == "multi-paragraph":
+        section_data = model_data["paragraphs"]
+        return convert_content(title, section_data)
+
+    if model_type == "gallery":
+        # ignore
+        return ""
+
+    return f"<div>unhandled model type: {model_type}<br/>{model_data}</div>\n"
+
+
+def sleep_random(max_seconds):
+    rand_secs = random.randint(1, max_seconds)  # nosec
+    time.sleep(rand_secs)
+
+
+# ============================================================
+
+
+def get_generator(_gen_params=None) -> RSSGenerator:
+    return UMUrsynowGenerator()
---
+++ src/testrssforward/site/intgr_umursynow.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+#
+# MIT License
+#
+# Copyright (c) 2021 Arkadiusz Netczuk <dev.arnet@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+try:
+    ## following import success only when file is directly executed from command line
+    ## otherwise will throw exception when executing as parameter for "python -m"
+    # pylint: disable=W0611
+    import __init__
+except ImportError:
+    ## when import fails then it means that the script was executed indirectly
+    ## in this case __init__ is already loaded
+    pass
+
+import sys
+import logging
+import pprint
+
+from rssforward import logger
+from rssforward.rssgenerator import RSSGenerator
+from rssforward.site.umursynow import get_generator, get_content, MAIN_NAME, get_news_links
+from rssforward.utils import write_data
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def grab_by_generator():
+    params = {}
+    generator: RSSGenerator = get_generator(params)
+    gen_data = generator.generate()
+    pprint.pprint(gen_data)
+
+
+def main():
+    logger.configure_console()
+
+    # grab_by_generator()
+
+    links = get_news_links()
+    if not links:
+        print("FAILED")
+        sys.exit(1)
+
+    out_path = f"/tmp/{MAIN_NAME}.html"
+    offers_content = get_content(1, html_output=out_path)
+    if not offers_content:
+        print("FAILED")
+        sys.exit(1)
+
+    out_path = f"/tmp/{MAIN_NAME}.xml"
+    _LOGGER.info("writing rss to file: file://%s", out_path)
+    write_data(out_path, offers_content)
+
+
+if __name__ == "__main__":
+    main()
